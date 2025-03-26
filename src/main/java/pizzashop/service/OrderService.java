1c7afd0b0a8c7d9a53a3d4768bf0b181a1452adb
anda03maria <114091652+anda03maria@users.noreply.github.com>
2025-03-26 15:26:53 +0200

    Add files via upload

--- src/main/java/pizzashop/service/OrderService.java
+++ src/main/java/pizzashop/service/OrderService.java
@@ -24,6 +24,14 @@ public class OrderService {
 
     public void addPayment(int table, PaymentType type, double amount){
         Payment payment= new Payment(table, type, amount);
+        if(amount <= 0){
+            throw new IllegalArgumentException("Amount must be greater than zero");
+        } else if(type == null){
+            throw new IllegalArgumentException("Payment type cannot be null");
+
+        } else if(table < 1 || table >8){
+            throw new IllegalArgumentException("Table number must be between 1 and 8");
+        }
         payRepo.add(payment);
     }
 
