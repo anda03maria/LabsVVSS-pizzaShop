a3cefb8bd566c5f2a469c412f7e5b20ca9fd260f
teocheta <129763217+teocheta@users.noreply.github.com>
2025-04-30 17:01:51 +0300

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,1 +1,16 @@
+module pizzashop {
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires java.desktop;
+
+    opens pizzashop.model to javafx.base;
+    exports pizzashop.model;
+    opens pizzashop to javafx.fxml;
+    exports pizzashop;
+    opens pizzashop.controller to javafx.fxml;
+    exports pizzashop.controller;
+    exports pizzashop.repository to org.mockito; // sau to ALL-UNNAMED
+    requires org.mockito;
+}
 
